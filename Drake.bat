69ebd93ae6436703b588934070eedc13024073b8
Scott Killen <Scott@scottkillen.com>
2020-08-03 07:42:39 -0400

    :bug: Correct log name format

--- Drake.bat
+++ Drake.bat
@@ -1,6 +1,6 @@
 SET SRC="C:\Drake"
 SET DEST="C:\Users\scott\Dropbox (KillenCPA)\KANAS\Drake Backups"
-SET YMD=%DATE:~10,4%-%DATE:~7,2%-%DATE:~4,2%
+SET YMD=%DATE:~10,4%-%DATE:~4,2%-%DATE:~7,2%
 SET LOG=C:\Users\scott\BackupJobs\logs\Drake--%YMD%.txt
 SET OPT=/MIR /NP /R:5 /MT:32 /LOG+:%Log% /TS /FP
 SET CMD=robocopy %SRC% %DEST% %OPT%
