0c00b428eb9fde5d2567e0a553404a12101478de
Scott Killen <Scott@scottkillen.com>
2020-08-11 08:15:22 -0400

    Mask informational error codes.

--- Drake.bat
+++ Drake.bat
@@ -4,4 +4,4 @@ SET YMD=%DATE:~10,4%-%DATE:~4,2%-%DATE:~7,2%
 SET LOG=C:\Users\scott\BackupJobs\logs\Drake--%YMD%.txt
 SET OPT=/MIR /NP /R:5 /MT:32 /LOG+:%Log% /TS /FP
 SET CMD=robocopy %SRC% %DEST% %OPT%
-%CMD%
+(%CMD%) ^& IF %ERRORLEVEL% LSS 8 SET ERRORLEVEL = 0
